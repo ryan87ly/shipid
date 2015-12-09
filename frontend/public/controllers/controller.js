6c56f746642ef10289dd23a40f2069932493b074
Matthew Ho <matthew.ho@ullink.com>
2015-12-09 20:12:27 +0800

    - change plugin color according to status

--- frontend/public/controllers/controller.js
+++ frontend/public/controllers/controller.js
@@ -59,6 +59,14 @@ app.controller('frondendCtrl', ['$scope', '$http', 'socket', function ($scope, $
   	 console.log("send message: " + JSON.stringify(msg));
      socket.emit("message", msg);
   }
+  
+    $scope.checkColor = function(status) {
+    console.log("checkColor : " + status);
+    if (status == "on") {
+        return "green";
+    }
+    return "red";
+  }
 
   $scope.checkSendMessage = function(msg) {
     console.log("check " + JSON.stringify(msg));
